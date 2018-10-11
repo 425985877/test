b0b5b2a41a3aaf5a78095bedd266ea7f3d11a040
Your Name <Zhao Shen>
2018-10-11 14:43:02 +0800

    add quicj sort

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,9 +1,16 @@
 public class BinarySearch {
     public static void main(String [] args) {
         int a[] = {1, 2, 3, 4, 5, 6};
+        int b[]={32,3,1,43,23,54,21};
         BinarySearch binarySearch = new BinarySearch();
-        System.out.println(binarySearch.binarySearch(a, 3, 0, a.length - 1));
-        System.out.println(binarySearch.binarySearchBycycle(a, 3));
+        //System.out.println(binarySearch.binarySearch(a, 3, 0, a.length - 1));
+       // System.out.println(binarySearch.binarySearchBycycle(a, 3));
+        binarySearch.quickSort(b,0,b.length-1);
+        System.out.println(b);
+        for(int i=0;i<b.length;i++){
+            System.out.print(b[i]+" ");
+        }
+
     }
     public int binarySearch(int [] a,int b,int start,int end){
         int mid=(start+end)/2;
@@ -28,7 +35,34 @@ public class BinarySearch {
             mid=(start+end)/2;
         }
         return mid;
+    }
+    public void quickSort(int [] a,int start,int end){
+        if(start>=end){
+            return;
+        }
+        int left=start;
+        int right=end;
+        int mid=start;
+        while (left<right){
+            while (a[mid]<a[right]){
+                right--;
+            }
+            swap(a,mid,right);
+            mid=right;
+            while (a[mid]>a[left]){
+                left++;
+            }
+            swap(a,mid,left);
+            mid=left;
+        }
+        quickSort(a,start,mid-1);
+        quickSort(a,mid+1,end);
 
+    }
+    public void swap(int [] array,int a,int b){
+        int temp=array[b];
+        array[b]=array[a];
+        array[a]=temp;
     }
 
 }
